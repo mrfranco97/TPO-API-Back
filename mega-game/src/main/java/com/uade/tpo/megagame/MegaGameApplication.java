fd700b826c9f7340392e51b16451f400a84ae3f8
MrFranco <50271790+mrfranco97@users.noreply.github.com>
2024-05-17 14:32:28 -0300

    Add files via upload
    Se agrega la estructura de springboot

---
+++ mega-game/src/main/java/com/uade/tpo/megagame/MegaGameApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.megagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaGameApplication.class, args);
+	}
+
+}
