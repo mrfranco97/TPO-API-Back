4df621b29eb505af2577f9f51998433d212a3ef9
Luciano Arias <luciano.arias@sharksistemas.com.ar>
2024-05-27 16:58:48 -0300

    entity tipo

---
+++ mega-game/src/main/java/com/uade/tpo/megagame/entity/Tipo.java
@@ -0,0 +1,24 @@
+package com.uade.tpo.megagame.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Tipo {
+    public Tipo(){
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_tipo;
+   
+    @Column
+    private String nombre;
+    
+}
