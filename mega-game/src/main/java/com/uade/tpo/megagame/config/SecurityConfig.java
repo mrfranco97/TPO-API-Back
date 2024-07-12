2466404fdf479d5cf079b188febe9deac74a0ab9
MrFranco <50271790+mrfranco97@users.noreply.github.com>
2024-07-12 15:40:30 -0300

    Se permite la venta a todos

--- mega-game/src/main/java/com/uade/tpo/megagame/config/SecurityConfig.java
+++ mega-game/src/main/java/com/uade/tpo/megagame/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                     .requestMatchers("/api/v1/auth/**").permitAll()
                     .requestMatchers("/abm/**").hasAnyAuthority(Role.ADMIN.name())
                     .requestMatchers("/catalogo/**").permitAll()
+                    .requestMatchers("/ventas/**").permitAll()
                     /* 
                     .requestMatchers("/error/**").permitAll()
                     // Requiere el rol USER para todas las solicitudes a /catalogo/**
